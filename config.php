de9b45402ccb72254e8abc77f90f71ad69ca8ea8
AbangRombeng <80770646+SUBUR78990@users.noreply.github.com>
2022-06-29 10:27:47 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+//Anda Cari di https://games.shopee.co.id/farm/api/orchard/
+//atau cari yang link web nya berhubungan dengan kata FARM
+$userAgent     = "Mozilla/5.0 (Linux; Android 8.1.0; vivo 1811 Build/OPM1.171019.026; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/99.0.4844.58 Mobile Safari/537.36 Shopee Beeshop locale/id version=600 appver=28532 rnver=1649775583 app_type=1";
+$spc_clientid  = "IdeQBu4MWccQIBDTcsevmikginuzyamh";
+$spc_f         = "833795762518717_c79820ac653b9899_B6C9521EA07DF348";
+$spc_aftid     = "54b24d72-5798-4475-bb65-fa7f0aa214f4";
+$userid        = "57886591";
+$username      = "subur_martiana";
+$spc_u         = "57886591";
+$csrftoken     = "6lAf2WLvhxul2gC2ieCVf8BuHsAJQ8es";
+$shopee_token  = "DkA6TNXT1SoVPCuhWq3rU6xXuo2rEht0Lb2/32CYIfM67qwUDkawT2iCrnZcollV";
+$spc_r_t_id	   = 'W+W9TCxKiuOc2uv1LMGBrmpNm02Zf4cXhAu/7SzwyDTIhYMyXesF2wQlgB7Bar+AKb/nLt/Q1g2u6/Twpc+/87nKrChfu6bYAfGMSx8nakU';
+$spc_r_t_iv	   = '1/Jgk0CM2zBRQyNNU5HfuA';
+$spc_ec 	   = 'ZkRHS1FoaWNZbE94aUVTMBbGOSrHG5lg5w8Wb+XGG+A4znfz9qf+m2qg3mlH8BBiaRZ1f99xF6ou9MD/n18QklEd2av2YrSnQ7aXVfNwBdw7uSk1qP641aqzPfcrces3lUnlKu8qUeO3rc/SyygtO8V6auKb+ofnO+siiNygh7g';
+$ga            = "1.1.1418956013.1647194731";
+$shopee_app_version = "28532";
+$shopee_rn_version  = "1649775583";
+$spc_si             = "rU1EYgAAAABVS21qYWhsQQ5frAAAAAAAZ2g1VGNkMG0";
+$UA                 = "Shopee%20Android%20Beeshop%20locale%2Fid%20version%3D600%20appver%3D28532";//berada paling bawah
+
+
+
+//Data Penyiraman
+//Anda Cari di https://games.shopee.co.id/farm/api/orchard/crop/water
+
+//contoh
+//$data_siram = '{"cropId":13083794,"resourceId":8220864}';
+$data_siram = '{"pageName":"groceryStore"}'; 
+$UA_siram   = "Shopee%20Android%20Beeshop%20locale%2Fid%20version%3D600%20appver%3D28532";//berada paling bawah
